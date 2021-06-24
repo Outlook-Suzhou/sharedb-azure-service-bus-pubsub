17aa2017a879767bbce9904f3551739953f10918
Fangzhou Lin <fanglin@microsoft.com>
2021-06-24 09:12:05 +0800

    add more

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,20 @@ export class AzureServiceBusPubSub extends PubSub {
   private busclient: BusClient
 
   protected _subscribe(channel: string, callback: (err: Error) => void): void {
-    let messageHandler = async (data: any) => {
-      this._emit(channel, data.data);
-    }
-
-    let errorHandler = async (error : any) => {
-      console.error(error)
-    }
-
-    this.busclient.subscribe(channel, messageHandler, errorHandler)
-    .then(() => callback(null)).catch((e) => callback(e));
+    this.busclient.createTopicIfNotExist(channel)
+    .then(() => {
+      let messageHandler = async (data: any) => {
+        this._emit(channel, data.data);
+      }
+  
+      let errorHandler = async (error : any) => {
+        console.error(error)
+      }
+  
+      this.busclient.subscribe(channel, messageHandler, errorHandler)
+      .then(() => callback(null))
+      .catch((e) => callback(e));
+    }).catch(e => callback(e));
   }
 
   protected _unsubscribe(channel: string, callback: (err: Error) => void): void {
