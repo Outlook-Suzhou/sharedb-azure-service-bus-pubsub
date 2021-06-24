d99cbec9fa0176d671381a9d09d6384c10329585
Fangzhou Lin <fanglin@microsoft.com>
2021-06-24 08:30:05 +0800

    add more

--- src/index.ts
+++ src/index.ts
@@ -1,81 +1,26 @@
 import { ServiceBusClient } from "@azure/service-bus";
-const PubSub = require('sharedb').PubSub;
+import {PubSub} from 'sharedb'
+import * as ShareDB from 'sharedb';
+import { Error } from "sharedb/lib/sharedb";
+import { BusClient } from "./servicebus";
 
-// AzureServiceBus pubsub driver for ShareDB.
-function AzureServiceBusPubSub(options) : void {
-  if (!(this instanceof AzureServiceBusPubSub)) return new AzureServiceBusPubSub(options);
-  PubSub.call(this, options);
-  options || (options = {});
 
-  this.client = options.client ||  new ServiceBusClient(options.connectionString);
-  this.receiverMap = {};
-}
+export class AzureServiceBusPubSub extends PubSub {
+  private busclient: BusClient
 
-module.exports = AzureServiceBusPubSub;
-
-AzureServiceBusPubSub.prototype = Object.create(PubSub.prototype);
-
-AzureServiceBusPubSub.prototype.close = function(callback) {
-  if (!callback) {
-    callback = function(err) {
-      if (err) throw err;
-    };
+  protected _subscribe(channel: string, callback: (err: Error) => void): void {
+    throw new Error("Method not implemented.");
   }
-  var pubsub = this;
-  PubSub.prototype.close.call(this, async function(err) {
-    if (err) return callback(err);
-    for (const [key, value] of pubsub.receiverMap.entries()) {
-      if(value)
-      {
-        await value.close();
-      }
-    }
-    await PubSub.client.close()
-  });
-};
-
-AzureServiceBusPubSub.prototype._subscribe = function(channel, callback) {
-  var pubsub = this;
-  const myMessageHandler = async (messageReceived) => {
-		var data = JSON.parse(messageReceived);
-    pubsub._emit(channel, data);
-	};
-
-	// function to handle any errors
-	const myErrorHandler = async (error) => {
-		console.log(error);
-	};
-  const receiver = this.client.createReceiver(channel);
-  receiver.subscribe({
-		processMessage: myMessageHandler,
-		processError: myErrorHandler
-	});
-
-  this.receiverMap[channel] = receiver;
-  callback();
-};
-
-AzureServiceBusPubSub.prototype._unsubscribe = async function(channel, callback) {
-  if(this.receiverMap[channel])
+  protected _unsubscribe(channel: string, callback: (err: Error) => void): void {
+    throw new Error("Method not implemented.");
+  }
+  protected _publish(channels: string[], data: any, callback: (err: Error) => void): void {
+    throw new Error("Method not implemented.");
+  }
+  constructor(connectString: string, options?: any)
   {
-    await this.receiverMap[channel].close();
+    super(options);
+    this.busclient = new BusClient(connectString);
   }
-  callback();
-};
 
-AzureServiceBusPubSub.prototype._publish = async function(channels, data, callback) {
-  // Todo we need a buffer
-  const sender = this.client.createSender(channels);
-  try {
-    let batch = await sender.createMessageBatch();
-    if (batch.tryAddMessage(data)) {
-      await sender.sendMessages(batch);
-    } else {
-      callback("Message too big to fit in a batch");
-    }
-  }
-  finally{
-    await sender.close();      
-  }
-  callback();
-};
+}
